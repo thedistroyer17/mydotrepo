186dd767fb2d7e3a0fdb50fe272b16eb0aef6c46
darkxx <arpit2003y@gmail.com>
2023-05-26 17:56:21 +0530

    added fish shell config files

---
+++ .config/fish/config.fish
@@ -0,0 +1,16 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set fish_greeting
+
+figlet -f new "#darkxx"
+
+
+starship init fish | source
+
+#git bare repository control alias 
+function dotfiles
+  command git --git-dir=$HOME/dotfiles.git --work-tree=$HOME $argv
+end
+
