dbdde5fcda9497834fa06eeca134b3c025f56b06
arpit <arpit2003y@gmail.com>
2023-06-06 20:00:56 +0530

    changes changes and changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -27,3 +27,5 @@ end
 alias vim="nvim"
 alias chrome="google-chrome-stable"
 
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/darkxx/.ghcup/bin $PATH # ghcup-env
