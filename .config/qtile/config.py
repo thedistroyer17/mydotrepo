fac6c8357aaf47510d421e6fe1c4c1d041a669a4
arpit <arpit2003y@gmail.com>
2023-06-12 22:52:46 +0530

    new changes

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -1,27 +1,63 @@
- #######  ######## #### ##       ########     ######   #######  ##    ## ######## ####  ######   
-##     ##    ##     ##  ##       ##          ##    ## ##     ## ###   ## ##        ##  ##    ##  
-##     ##    ##     ##  ##       ##          ##       ##     ## ####  ## ##        ##  ##        
-##     ##    ##     ##  ##       ######      ##       ##     ## ## ## ## ######    ##  ##   #### 
-##  ## ##    ##     ##  ##       ##          ##       ##     ## ##  #### ##        ##  ##    ##  
-##    ##     ##     ##  ##       ##          ##    ## ##     ## ##   ### ##        ##  ##    ##  
- ##### ##    ##    #### ######## ########     ######   #######  ##    ## ##       ####  ######   
+
+###########################
+##       _   _ _         ##
+##      | | (_) |        ##
+##  __ _| |_ _| | ___    ##
+## / _` | __| | |/ _ \   ##
+## | (_| | |_| | |  __/   ##
+## \__, |\__|_|_|\___|   ##
+##    | |                ##
+##    |_|                ##
+###########################
+
 
 ## imports ##
+import os
 import subprocess
-from libqtile import bar, layout, widget 
+from libqtile import bar, layout, widget, hook
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
-from battery_widget import BatteryWidget 
+from battery_widget import BatteryWidget
 from libqtile.widget import CPUGraph
 
 mod = "mod4"
 terminal = "kitty"
 
 # running dmenu
-#  def spawn(cmd):
-    #  qtile.cmd_spawn("dmenu_run -p 'Run:' -nb '#1a1a1a' -nf '#dcdccc' -sb '#8f8f8f' -sf '#dcdccc' -fn 'Monospace-14'")  # Replace with your preferred dmenu command
+# def spawn_dmenu(cmd):
+#       qtile.cmd_spawn("dmenu_run -p 'Run:' -nb '#1a1a1a' -nf '#dcdccc' -sb '#8f8f8f' -sf '#dcdccc' -fn 'Monospace-14'")  # Replace with your preferred dmenu command
 
+# brigtness control
+
+
+# @hook.subscribe.startup_once
+# def autostart():
+#     # Set initial brightness level here if needed
+#     pass
+
+
+# def change_brightness(delta):
+#     current_brightness = int(
+#         float(subprocess.check_output(["xbacklight", "-get"])))
+#     new_brightness = max(current_brightness + delta, 0)
+#     subprocess.Popen(["xbacklight", "-set", str(new_brightness)])
+
+
+# def increase_brightness(qtile):
+#     change_brightness(10)
+
+
+# def decrease_brightness(qtile):
+#     change_brightness(-10)
+
+
+# def toggle_brightness(qtile):
+#     subprocess.Popen(["xbacklight", "-toggle"])
+
+
+# picom setup
+os.system("picom --config ~/.config/picom/picom.conf &")
 
 
 keys = [
@@ -32,17 +68,22 @@ keys = [
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+    Key([mod], "space", lazy.layout.next(),
+        desc="Move window focus to other window"),
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left(),
+        desc="Move window to the left"),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right(),
+        desc="Move window to the right"),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key([mod, "control"], "h", lazy.layout.grow_left(),
+        desc="Grow window to the left"),
+    Key([mod, "control"], "l", lazy.layout.grow_right(),
+        desc="Grow window to the right"),
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
@@ -62,15 +103,20 @@ keys = [
     Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
     Key([mod, "shift"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod], "x", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "d", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
-    # Key([mod], "d", lazy.spawn, args=[None], desc="Launch dmenu"),
+    # Key([mod], "d", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    Key([mod], "d", lazy.spawn("dmenu_run"), desc="launch dmenu"),
+    # Key([mod], "XF86MonBrightnessUp", lazy.function(increase_brightness)),
+    # Key([mod], "XF86MonBrightnessDown", lazy.function(decrease_brightness)),
+
 ]
+
 
 groups = []
 
 group_names = ["1", "2", "3", "4", "5", "6",]
 group_labels = ["TERM", "WWW", "SYS", "DEV", "FILE", "GFX",]
-group_layouts = ["monadtall", "monadtall", "monadtall", "monadtall", "monadtall", "monadtall",]
+group_layouts = ["monadtall", "monadtall", "monadtall",
+                 "monadtall", "monadtall", "monadtall",]
 
 
 for i in range(len(group_names)):
@@ -84,11 +130,12 @@ for i in groups:
     keys.extend([
         Key([mod], i.name, lazy.group[i.name].toscreen()),
         Key([mod], "Tab", lazy.screen.next_group()),
-        Key([mod, "shift" ], "Tab", lazy.screen.prev_group()),
+        Key([mod, "shift"], "Tab", lazy.screen.prev_group()),
         Key(["mod1"], "Tab", lazy.screen.next_group()),
         Key(["mod1", "shift"], "Tab", lazy.screen.prev_group()),
 
-        Key([mod, "shift"], i.name, lazy.window.togroup(i.name) , lazy.group[i.name].toscreen()),
+        Key([mod, "shift"], i.name, lazy.window.togroup(
+            i.name), lazy.group[i.name].toscreen()),
     ])
 
 
@@ -96,12 +143,13 @@ layouts = [
     # layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
     # layout.Max(),
     # Try more layouts by unleashing below layouts.
-    #layout.Stack(num_stacks=2 , border_focus="#fc05ec",border_normal="#9e8d9d",border_width=2),
+    # layout.Stack(num_stacks=2 , border_focus="#fc05ec",border_normal="#9e8d9d",border_width=2),
     # layout.Bsp(),
     # layout.Matrix(),
-    layout.MonadTall(border_focus="#fc05ec",border_normal="#9e8d9d",border_width=2, margin=7),
+    layout.MonadTall(border_focus="#fc05ec",
+                     border_normal="#9e8d9d", border_width=2, margin=7),
     # layout.MonadWide(),
-    #layout.RatioTile(border_focus="#fc05ec", border_normal="#9e8d9d",border_width=2),
+    # layout.RatioTile(border_focus="#fc05ec", border_normal="#9e8d9d",border_width=2),
     # layout.Tile(),
     # layout.TreeTab(),
     # layout.VerticalTile(),
@@ -120,7 +168,7 @@ separator_widget = widget.TextBox(
     text="|",
     fontsize=14,
     padding=5,
-    foreground="888888",
+    foreground="#ffc0cb",
     background="#1e1e2e"
 )
 
@@ -148,7 +196,7 @@ screens = [
                 separator_widget,
                 widget.Net(interface="wlan0"),
                 separator_widget,
-                widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
+                widget.Clock(format="%H:%M %p"),
                 separator_widget,
                 CPUGraph(),
                 separator_widget,
@@ -160,14 +208,16 @@ screens = [
             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
             # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
             background="#1e1e2e"
-        ) 
+        )
     ),
 ]
 
 # Drag floating layouts.
 mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Drag([mod], "Button1", lazy.window.set_position_floating(),
+         start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(),
+         start=lazy.window.get_size()),
     Click([mod], "Button2", lazy.window.bring_to_front()),
 ]
 
@@ -208,5 +258,5 @@ wl_input_rules = None
 # We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
 # java that happens to be on java's whitelist.
 wmname = "LG3D"
-#wallaper setting using the ntriogen program 
-subprocess.Popen(["nitrogen","--restore"])
+# wallaper setting using the ntriogen program
+subprocess.Popen(["nitrogen", "--restore"])
